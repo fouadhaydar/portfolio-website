36e2edc3aabfabcb3b7ff2fdaa00d1b54db73406
Haidar <haidar@Haidars-MacBook-Pro.local>
2023-01-07 15:12:34 +0200

    fix reesponsive issue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // make the nav bar responsive
 let bars = document.getElementById('bars')
 let ulContainer = document.getElementById('content')
+let h2s = document.querySelectorAll('#h2')
 
 window.addEventListener("resize", function() {
   let windowWidth = window.innerWidth;
@@ -10,8 +11,28 @@ window.addEventListener("resize", function() {
   else {
     ulContainer.style.height = '0px'
   }
+  if(windowWidth < 750) {
+    h2s.forEach(h2 => {
+      h2.style.display = 'none'
+    });
+  }
+  else {
+    h2s.forEach(h2 => {
+      h2.style.display = 'block'
+    });
+  }
 });
-
+let windowWidth = window.innerWidth
+if(windowWidth < 750) {
+  h2s.forEach(h2 => {
+    h2.style.display = 'none'
+  });
+}
+else {
+  h2s.forEach(h2 => {
+    h2.style.display = 'block'
+  });
+}
 bars.addEventListener('click',()=>{
     let closed = ulContainer.offsetHeight
     console.log(closed)
